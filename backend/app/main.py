c3a7e357fe7ab113db91baaac6c88b8f91c22d68
Tenzin Jamyang <JamyanT@ci.richmond.ca.us>
2026-06-25 16:26:55 -0700

    added few comments

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,10 +12,15 @@ app = FastAPI()
 
 # This function gives each API request its own database session.
 # The session opens before the request runs and closes after the request finishes.
+"""
+Function creates a new session everytime its called
+Session opens before each new api request, then closes afterwards.
 
+"""
 def get_db():
     db = SessionLocal()
-
+    
+    #after creating new session, stays open until request is finished in route using yield, then closes
     try: 
         yield db 
     finally: 
@@ -25,14 +30,17 @@ def get_db():
 def read_root():
     return {"message": "Cardfolio backend is running"}
 
-'''
-When someone sends a POST request to /cards,
-use CardCreate to check the incoming data,
-save it as a Card database model,
-then return it using CardResponse.'''
+#Creates the API route when someone sends a POST request to /cards
 @app.post("/cards", response_model = CardResponse)
+#
 def create_card(card: CardCreate, db: Session = Depends(get_db)):
-       # Convert the incoming CardCreate schema into a SQLAlchemy Card model.
+    '''
+    When someone sends a POST request to /cards,
+    use CardCreate to check the incoming data,
+    save it as a Card database model,
+    then return it using CardResponse.
+    '''   
+    # Convert the incoming CardCreate schema into a SQLAlchemy Card model.
     db_card = Card(**card.model_dump())
 
     #Stage the new card to be saved.
@@ -46,27 +54,28 @@ def create_card(card: CardCreate, db: Session = Depends(get_db)):
 
     # Return the saved card
     return db_card
-"""
-When someone sends a GET request to /cards,
-query the database for all saved card records,
-then return them as a list using CardResponse.
-"""
+
 @app.get("/cards", response_model=list[CardResponse])
 def get_cards(db: Session = Depends(get_db)):
+    """
+    When someone sends a GET request to /cards,
+    query the database for all saved card records,
+    then return them as a list using CardResponse.
+    """    
     # Query the database for all Card rows.
     cards = db.query(Card).all()
 
     # Return the list of cards.
     return cards
 
-"""
-When someone sends a GET request to /cards/{card_id},
-search the database for one card with that id,
-then return the matching card.
-"""
+
 @app.get("/cards/{card_id}", response_model=CardResponse)
 def get_card(card_id: int, db: Session = Depends(get_db)):
-
+    """
+    When someone sends a GET request to /cards/{card_id},
+    search the database for one card with that id,
+    then return the matching card.
+    """
     card = db.query(Card).filter(Card.id == card_id).first()
     #if card isn't found throw error
     if card is None:
