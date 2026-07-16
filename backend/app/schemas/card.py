129932810bd531e5741f9ea95363f53f9d1266c7
Tenzin Jamyang <tenzinjamyang121@gmail.com>
2026-07-16 15:33:21 -0700

    added Field to card properties. set base values and logic

--- backend/app/schemas/card.py
+++ backend/app/schemas/card.py
@@ -1,12 +1,19 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict,Field
 
+'''
+The new Field rules mean:
+
+name must contain at least 1 character
+name cannot exceed 100 characters
+rarity and condition cannot exceed 50 characters
+price cannot be negative'''
 
 class CardBase(BaseModel):
     #Newly created card must have a name
-    name:str
-    rarity: str | None = None
-    condition: str | None = None
-    price: float | None = None
+    name:str = Field(min_length = 1, max_length = 100)
+    rarity: str | None = Field(default = None, max_length = 50)
+    condition: str | None = Field(default = None, max_length = 50)
+    price: float | None = Field(default = None, ge = 0)
 
     '''User sends a Json -> Cardcreate checks the incoming data-> Card Model turns it into a dataBase Object
        SQLAlchemy saves it to SQLite - > CardResponse controls what gets sent back.
@@ -16,10 +23,11 @@ class CardCreate(CardBase):
 
 class CardUpdate(CardBase):
     #Everything is optional, since user can update any value
-    name: str | None= None
-    rarity: str | None = None
-    condition: str | None = None
-    price: float | None = None
+    name: str | None = Field(default=None, min_length=1, max_length=100)
+    rarity: str | None = Field(default=None, max_length=50)
+    condition: str | None = Field(default=None, max_length=50)
+    price: float | None = Field(default=None, ge=0)
+
 
 class CardResponse(CardBase):
     id: int
