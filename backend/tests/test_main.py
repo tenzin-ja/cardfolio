5a1a65cffcd9c112d8f2ddaf0bbd4e8970c9b168
Tenzin Jamyang <tenzinjamyang121@gmail.com>
2026-08-05 14:45:22 -0700

    created test for new rule

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -401,3 +401,20 @@ def test_card_variant_rejects_duplicate_variant_for_same_catalog_card():
         with pytest.raises(IntegrityError):
             db.commit()
         db.rollback()
+
+#testing hook succesfully turns on foreignkey, and rejects variant with nonexistant id
+def test_card_variant_rejects_missing_catalog_card():
+
+       
+    first_variant = CardVariant (
+        catalog_card_id = 9999,
+        variant_key = "holofoil"
+    )
+    with TestingSessionLocal() as db:
+
+        db.add(first_variant)
+
+        with pytest.raises(IntegrityError):
+            db.commit()
+
+        db.rollback()    
