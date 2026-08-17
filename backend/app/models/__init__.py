60fc0f3fb85eebf94a13b2cfd0764788a7c8928f
Tenzin Jamyang <tenzinjamyang121@gmail.com>
2026-08-17 15:41:33 -0700

    small fixes

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,10 +2,12 @@ from app.models.card import Card
 from app.models.catalog_card import CatalogCard
 from app.models.card_variant import CardVariant
 from app.models.collection_item import CollectionItem
+from app.models.price_snapshot import PriceSnapshot
 
 __all__ = [
     "Card",
     "CatalogCard",
     "CardVariant",
     "CollectionItem",
+    "PriceSnapshot"
 ]
